dae23f094650212a45ab9c430212e98aca710c51
Schubert Araujo <xubsoldier@gmail.com>
2023-12-29 15:39:52 -0300

    Update .prettierrc.js

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   arrowParens: 'always',
   bracketSpacing: true,
-  endOfLine: 'crlf',
+  endOfLine: 'cr',
   htmlWhitespaceSensitivity: 'ignore',
   insertPragma: false,
   jsxSingleQuote: false,
